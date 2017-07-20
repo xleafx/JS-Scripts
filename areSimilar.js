fdf305031514d6637215fdb386d3653e3d729ddb
edge <alextyupa@gmail.com>
2017-07-20 10:42:02 +0300

    Wednesday

--- areSimilar.js
+++ areSimilar.js
@@ -24,24 +24,31 @@
 
 function areSimilar(A, B) {
 
-  for(var i = 0; i < A.length; i++) {
-    for(var j = i; j < A.length; j++) {
-      var equal = true,
-        tmp = A[i];
-      A[i] = A[j];
-      A[j] = tmp;
-      for(var k = 0; k < A.length; k++) {
-        if(A[k] !== B[k]) {
-          equal = false;
-          break;
-        }
-      }
-      if(equal) {
-        return true;
-      }
-      A[j] = A[i];
-      A[i] = tmp;
+  if(A.length !== B.length){
+    return false
+  }
+  var fA, fB
+  for(var i = 0; i < A.length; i++){
+    if(A[i] !== B[i]){
+      var misMatch = i
+      i = Infinity;
     }
   }
-  return false;
+  for(var j = A.length -1; j >= 0; j--){
+    if(A[j] !== B[j]){
+      if(A[misMatch] === B[j] && B[misMatch] === A[j]){
+        var temp = B[misMatch]
+        B[misMatch] = B[j]
+        B[j] = temp
+      }else {
+        return false
+      }
+    }
+  }
+  for(var x = 0; x < A.length; x++){
+    if(A[x] !== B[x]){
+      return false
+    }
+  }
+  return true
 }
